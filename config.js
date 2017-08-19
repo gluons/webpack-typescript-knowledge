2301d363fd6986aca2f0938330dcd8e97d3e488c
Saran Tanpituckpong <sarunta@gmail.com>
2017-08-19 13:59:56 +0700

    :sparkles: Add docute

---
+++ config.js
@@ -0,0 +1,9 @@
+/* global docute */
+
+docute.init({
+	repo: 'gluons/webpack-typescript-knowledge',
+	'edit-link': 'https://github.com/gluons/webpack-typescript-knowledge/blob/master/',
+	nav: [
+		{ title: 'Home', path: '/' }
+	]
+});
